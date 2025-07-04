fb6ffb761171eb634ae61ab24d36cb5824eb8eaa
Andrey Gerasimenko <andreu2606@mail.ru>
2025-07-04 14:31:14 +0800

    The second attempt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+class MyClass
+{
+    int m_value;
+public:
+    MyClass(int value) : m_value(value) {} // Конструктор класса
+    int getValue() { return m_value; } // Метод класса для получения значения
+};
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
